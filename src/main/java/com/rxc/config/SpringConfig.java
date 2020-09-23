6667acec02ea0c6739abb7636690a194fa4d0554
RanXuCan <ranxucan@163.com>
2020-09-23 16:34:10 +0800

    Add a new class to replace the spring configuration file(beans.xml)

---
+++ src/main/java/com/rxc/config/SpringConfig.java
@@ -0,0 +1,15 @@
+package com.rxc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: 此类代替了Spring配置文件
+ * @Author RanXuCan
+ * @Date 2020/9/23 15:53
+ */
+@Configuration
+@ComponentScan(basePackages = {"com.rxc"})
+public class SpringConfig {
+
+}
